4026d43e691fe8959662dafca989b522f51d88b0
Adaeze Emelike <emelikepatience961@gmail.com>
2022-03-25 08:35:56 -0700

    Add files via upload

---
+++ index.html/main.js
@@ -0,0 +1,16 @@
+let saveEL = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+function increment () {
+    count += 1
+    countEl.textContent = count
+}
+
+function save () {
+    let countStr = count + " _ "
+    saveEL.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
